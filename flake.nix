29d83551986670c9511da6af05691430ff7e0092
joe <joe@example.org>
2025-10-01 11:31:14 +0200

    initial nix setup

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # HomeManager
+    # The `follows` keyword in inputs is used for inheritance.
+    # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+    # the `inputs.nixpkgs` of the current flake,
+    # to avoid problems caused by different versions of nixpkgs.
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+};
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+            home-manager.useUserPackages  = true;  # packages will be installed to /etc/profiles
+            home-manager.useGlobalPkgs = true;  # system level pkgs will be used
+            
+            home-manager.users.angelo = import ./home/home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home/home.nix
@@ -0,0 +1,80 @@
+{ config, pkgs, ... }:
+
+{
+    home = {
+        username = "angelo";
+        homeDirectory = "/home/angelo";
+        stateVersion = "25.05";
+
+        packages = with pkgs; [
+            btop
+            dconf2nix
+        ];
+    };
+
+
+    programs = {
+        gnome-shell = {
+          enable = true;
+          extensions = [{ package = pkgs.gnomeExtensions.gsconnect; }];
+        };
+        fish.enable = true;
+        git = {
+            enable = true;
+            userEmail = "joe@example.org";
+            userName = "joe";
+        };
+        vscode = {
+          enable = true;
+          #package = pkgs.vscodium;
+          profiles.default.extensions = with pkgs.vscode-extensions; [
+            vscodevim.vim
+            yzhang.markdown-all-in-one
+            ms-vscode.cpptools-extension-pack
+            ms-python.python
+            #kevinrose.vsc-python-indent
+            ms-python.debugpy
+            bbenoist.nix
+          ];
+        };
+        joplin-desktop = {
+          enable = true;
+          extraConfig = {
+            "sync.interval" = 300;  # sync every 5m
+            "sync.target" = 2;  # 2=filesystem
+            "sync.2.path" = "/home/angelo/Joplin";
+            "spellchecker.enabled" = true;
+          };
+        };
+    };
+
+    services = {
+        syncthing = {
+            enable = true;                 # turn on syncthing
+            #user = "angelo";               # run as your user
+            # home = "/home/angelo";         # config location
+            settings = {
+                options = {
+                    urAccepted = -1;  # dont share usage data
+                    #relaysEnabled = true;
+                    #localAnnounceEnabled = true;
+                };
+                devices = {
+                    "SM-A202F" = {
+                        id = "4R66KYU-GUZDHAT-HHH67JE-LJEECSR-GHQA3LW-4PYI7Z4-NTJUQB4-WQJRJA2";
+                        name = "SM-A202F";
+                    };
+                };
+                folders = {
+                    "/home/angelo/Joplin" = {
+                        id = "cyzeu-xl7em";
+                        devices = [ "SM-A202F" ];
+                        path = "/home/angelo/Joplin";
+                        type = "sendreceive";
+                        enable = true;
+                    };
+                };
+            };
+        };
+    };
+}
