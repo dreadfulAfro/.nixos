bd52fe6643d2d29e2eedfd8688ca4ab56bec1d0b
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-12 23:06:49 +0200

    redo mistake

--- hosts/nixos-server/system.nix
+++ hosts/nixos-server/system.nix
@@ -3,7 +3,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  #boot.initrd.luks.devices."luks-8e8ecfec-3711-4f6d-bd9f-fdf66f7396d8".device = "/dev/disk/by-uuid/8e8ecfec-3711-4f6d-bd9f-fdf66f7396d8";
+  boot.initrd.luks.devices."luks-8e8ecfec-3711-4f6d-bd9f-fdf66f7396d8".device = "/dev/disk/by-uuid/8e8ecfec-3711-4f6d-bd9f-fdf66f7396d8";
 
   # It may be necessary to wait a bit for devices to be initialized.
   # See https://github.com/NixOS/nixpkgs/issues/98741
