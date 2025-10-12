2b2a82b039def332242966666cdc2064720c9795
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-12 16:58:17 +0200

    fix path

--- users/admin/home.nix
+++ users/admin/home.nix
@@ -1,7 +1,7 @@
 {pkgs, username, ...}:
 {
   imports = [
-    ../../system/core.nix
+    ../../home/core.niix
   ];
 
   users.users.${username}.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvvzzmAtcKOcvRsdB28CAL9PVgeFwf44qiecDEUKY1C nixos-server" ];
