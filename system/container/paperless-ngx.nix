b1e43280602cea89f2d0d5ea50dd70c1aa567910
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-16 00:43:42 +0200

    added paperless container

---
+++ system/container/paperless-ngx.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}:
+{
+  containers.paperless-ngx = {
+    autoStart = true;
+
+    services.paperless = {
+      enable = true;
+      dataDir = "/srv/data1tb/paperless-ngx/data";
+      mediaDir = "/srv/data1tb/paperless-ngx/media";
+      passwordFile = "/etc/secrets/paperless/admin.key";
+      settings = {
+        PAPERLESS_OCR_LANGUAGE = "deu+eng";
+
+      };
+    }
+  }
+}
