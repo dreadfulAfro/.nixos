342074707354822edb35cf7b6779d79945b08723
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-15 15:19:48 +0200

    create symlink for .nixos

--- system/core.nix
+++ system/core.nix
@@ -19,7 +19,10 @@
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-    # Set your time zone.
+  # create symlink from ~/.nixos to /etc/nixos so i can use nixos-rebuild switch without arguments
+  environment.etc."nixos".target = "/home/admin/.nixos";
+
+  # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
