110a59d71c418a8ded68f0e50f47d2b335d641d4
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-15 13:33:50 +0200

    added encrypted data disk

---
+++ README.md
@@ -0,0 +1,25 @@
+# My NixOS Config
+
+## general
+### TODOs
+- [ ] figure out how to properly update etc.
+- [ ] system hardening, awesome-security
+- [ ] firewall setup
+- [ ] backup | redundancy
+
+## nixos-laptop
+
+## nixos-server
+### Services
+- [ ] paperless-ngx / paperlessAI?
+- [ ] Ansible?
+- [ ] BorgBackup
+- [ ] Kavita
+- [ ] librumreader
+- [ ] Linguacafe
+- [ ] Hortusfox
+- [ ] Jellyfin
+- [ ] obsidian-livesync
+- [ ] Recipe Sage | Meali | Tandoor Recipes | Tamari Recipe Manager
+- [ ] Tailscale
+- [ ] 
--- hosts/nixos-server/system.nix
+++ hosts/nixos-server/system.nix
@@ -16,6 +16,30 @@
 
  boot.initrd.luks.devices."luks-13c09ff4-cff9-4151-9a4e-976185f280f1".device = "/dev/disk/by-uuid/13c09ff4-cff9-4151-9a4e-976185f280f1";
 
+  # Initialize external drives
+  # 1TB Intenso
+  systemd.services."luks-open-data1tb" = {
+    description = "Unlock /srv LUKS volume 1TBIntenso";
+    wantedBy = [ "local-fs.target" ];  # systemd will try to run this unit when starting local filesystem
+    before = [ "local-fs.target" ];  # makes sure it is started before the local filesystem
+    serviceConfig = {
+      Type = "oneshot";  # unit is executed once
+      RemainAfterExit = true;   # is marked as still active after execution, so execStop is still called later
+      ExecStart = "${pkgs.cryptsetup}/bin/cryptsetup open /dev/disk/by-id/ata-ST1000LM024_HN-M101MBB_S2RXJ9ADB27331-part1 data1tb --key-file /etc/secrets/cryptsetup/ata-ST1000LM024_HN-M101MBB_S2RXJ9ADB27331-part1.key";
+      ExecStop = "${pkgs.cryptsetup}/bin/cryptsetup close data1tb";
+    };
+  };
+  # Mount the decrypted volume
+  fileSystems."/srv/data1tb" = {
+    device = "/dev/mapper/data1tb";
+    fsType = "ext4";
+  };
+  # Ensure /srv exists
+  systemd.tmpfiles.rules = [
+    "d /srv 0755 root root -"
+  ];
+
+
   # setup pre decryption ssh server
   boot.kernelParams = [ "ip=dhcp" ];
   boot.initrd = {
