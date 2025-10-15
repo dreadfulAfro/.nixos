be0cfb8ee01592de794f95dc3aaccc155709b5d1
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-16 01:08:14 +0200

    paperless

--- system/container/paperless-ngx.nix
+++ system/container/paperless-ngx.nix
@@ -1,17 +1,21 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
   containers.paperless-ngx = {
     autoStart = true;
 
-    services.paperless = {
-      enable = true;
-      dataDir = "/srv/data1tb/paperless-ngx/data";
-      mediaDir = "/srv/data1tb/paperless-ngx/media";
-      passwordFile = "/etc/secrets/paperless/admin.key";
-      settings = {
-        PAPERLESS_OCR_LANGUAGE = "deu+eng";
+    config =
+      { pkgs }:
+      {
+        services.paperless = {
+          enable = true;
+          dataDir = "/srv/data1tb/paperless-ngx/data";
+          mediaDir = "/srv/data1tb/paperless-ngx/media";
+          passwordFile = "/etc/secrets/paperless/admin.key";
+          settings = {
+            PAPERLESS_OCR_LANGUAGE = "deu+eng";
 
+          };
+        };
       };
-    };
   };
 }
