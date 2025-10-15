06346a7cb946c094a43c4129283ff244af60a677
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-15 22:31:48 +0200

    static ip

--- hosts/nixos-server/system.nix
+++ hosts/nixos-server/system.nix
@@ -42,6 +42,18 @@
     "d /srv 0755 root root -"
   ];
 
+  networking = {
+    interfaces = {
+      enp1s0.ipv4.addresses = [{
+        address = "192.168.178.57";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = {
+      address = "192.168.1.2";
+      interface = "enp1s0";
+    };
+  };
 
   # setup pre decryption ssh server
   #boot.kernelParams = [ "ip=dhcp" ];
