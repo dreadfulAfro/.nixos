2a16db5c793b3da1b43bda48a2c76d6d1a96c10b
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-18 00:18:42 +0200

    fixed handle path

--- system/container/caddy.nix
+++ system/container/caddy.nix
@@ -32,7 +32,8 @@
             "paperless.local, nixos-server.tail194e5d.ts.net" = {
               extraConfig = ''
                 tls internal
-                reverse_proxy /paperless 192.168.100.11:28981 {
+                handle_path /paperless/* {
+                reverse_proxy 192.168.100.11:28981 {
                   header_up X-Forwarded-Proto {scheme}
                   header_up Host {host}
                 }
