16f35885da01c04af7c29522ce60ead35d4cc0d6
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-20 23:09:32 +0200

    test

--- system/container/caddy.nix
+++ system/container/caddy.nix
@@ -2,6 +2,7 @@
 {
   containers.caddy = {
     autoStart = true;
+    user = "caddy";
     privateNetwork = true;
     hostAddress = "192.168.100.1";
     localAddress = "192.168.100.2";
@@ -29,14 +30,33 @@
           # email = "admin@example.org";
 
           virtualHosts = {
-            "paperless.local, nixos-server.tail194e5d.ts.net" = {
+            #"paperless.local, nixos-server.tail194e5d.ts.net" = {
+            #              extraConfig = ''
+            #                tls internal
+            #                handle_path /paperless/* {
+            #                  reverse_proxy 192.168.100.11:28981 {
+            #                    header_up X-Forwarded-Proto {scheme}
+            #                    header_up Host {host}
+            #                  }
+            #                }
+            #              '';
+            "paperless.local, paperless.nixos-server.tail194e5d.ts.net" = {
               extraConfig = ''
-                tls internal
-                handle_path /paperless/* {
-                  reverse_proxy 192.168.100.11:28981 {
-                    header_up X-Forwarded-Proto {scheme}
-                    header_up Host {host}
+                @tailscale host paperless.nixos-server.tail194e5d.ts.net
+                @lan host paperless.local
+
+                # LAN route with internal TLS
+                handle @lan {
+                  tls internal
+                  reverse_proxy 192.168.100.11:28981
+                }
+
+                # Tailscale route with Tailscale-provided certs
+                handle @tailscale {
+                  tls {
+                    get_certificate tailscale
                   }
+                  reverse_proxy 192.168.100.11:28981
                 }
               '';
             };
--- system/container/tailscale.nix
+++ system/container/tailscale.nix
@@ -8,14 +8,14 @@
   # Tailscale
   services.tailscale = {
     enable = true;
-    useRoutingFeatures = "server"; # or both
+    useRoutingFeatures = "both"; # or server
     authKeyFile = "/etc/secrets/tailscale/authKey.key";
     openFirewall = true;
     extraUpFlags = [
       "--advertise-routes=192.168.178.0/24, 192.168.100.0/24" # advertise your LAN subnet to Tailscale
       # "--ssh"                               # optional: allow SSH over Tailscale
     ];
-    permitCertUid = "root"; # allows Tailscale TLS certs
+    permitCertUid = "caddy"; # allows Tailscale TLS certs
 
   };
 }
