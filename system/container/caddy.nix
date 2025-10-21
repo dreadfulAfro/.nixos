6ea099f3f7a30a87cb32d9bda32948de25c2ef9a
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-21 23:13:54 +0200

    syntax error

--- system/container/caddy.nix
+++ system/container/caddy.nix
@@ -29,16 +29,15 @@
           # email = "admin@example.org";
 
           virtualHosts = {
-#            "nixos-server.tail194e5d.ts.net" = {
-#              extraConfig = ''
-#                tls internal
-#                reverse_proxy 192.168.100.11:28981 {
-#                    header_up X-Forwarded-Proto {scheme}
-#                    header_up Host {host}
-#                  }
-#                }
-#              '';
-#            };
+            "nixos-server.tail194e5d.ts.net" = {
+              extraConfig = ''
+                tls internal
+                reverse_proxy 192.168.100.11:28981 {
+                    header_up X-Forwarded-Proto {scheme}
+                    header_up Host {host}
+                }
+              '';
+            };
             "paperless.local" = {
               extraConfig = ''
                 tls internal
