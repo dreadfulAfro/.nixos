6c51a078f38f5e56165f1d8d9190a2683a4852d6
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-24 12:08:42 +0200

    asd

--- hosts/nixos-server/printserver.nix
+++ hosts/nixos-server/printserver.nix
@@ -3,20 +3,24 @@
   # publish printer on network
   services.printing = {
     enable = true;
-    webInterface = true;
-
     # This makes the server advertise printers via mDNS/IPP
     # so other devices can see them automatically.
-    browsed.enable = true;
+    #browsed.enable = true;
     listenAddresses = [ "0.0.0.0:631" ];
-    allowFrom = [ "all" ];
+    allowFrom = [ "192.168.178.0/24" ];
+    browsing = true;
     defaultShared = true;
+    openFirewall = true;
 
     # optional but neat: make Avahi advertise too
     # (for macOS/Linux autodiscovery)
   };
   services.avahi = {
     enable = true;
-    nssmdns4 = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
   };
 }
