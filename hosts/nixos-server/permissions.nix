1ad9c991dab2fae32486fdbf9f158e168a343c79
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-28 19:41:57 +0100

    permissions

--- hosts/nixos-server/permissions.nix
+++ hosts/nixos-server/permissions.nix
@@ -1,49 +1,49 @@
 { pkgs, ... }:
-
-let
-  mediaGid = 1000;
-
-  radarrUid = 1001;
-  sonarrUid = 1002;
-  lidarrUid = 1003;
-  jellyfinUid = 1004;
-in
 {
-  users.groups.media = {
-    gid = 1000;
+  users.groups = {
+    media = {
+      gid = 1000;
+    };
+    paperless = {};
   };
 
   # Create per-service users with shared group
-  users.users.radarr = {
-    uid = 1001;
-    group = "media";
-#    home = "/var/lib/radarr";
- #   shell = pkgs.runCommandShell "nologin" {};
-  };
+  users.users = {
+    radarr = {
+      isNormalUser = true;
+      uid = 1001;
+      group = "media";
+    };
   users.users.sonarr = {
+    isNormalUser = true;
     uid = 1002;
     group = "media";
 #    home = "/var/lib/sonarr";
 #    shell = pkgs.runCommandShell "nologin" {};
   };
   users.users.lidarr = {
+    isNormalUser = true;
     uid = 1003;
     group = "media";
 #    home = "/var/lib/lidarr";
 #    shell = pkgs.runCommandShell "nologin" {};
   };
   users.users.sabnzbd = {
+    isNormalUser = true;
     uid = 1004;
     group = "media";
   };
   users.users.jellyfin = {
+    isNormalUser = true;
     uid = 1005;
     group = "media";
 #    home = "/var/lib/jellyfin";
 #    shell = pkgs.runCommandShell "nologin" {};
   };
   users.users.paperless = {
+    isNormalUser = true;
     uid = 1006;
+    group = "paperless";
   };
 
   # Ensure media directory ownership & permissions
