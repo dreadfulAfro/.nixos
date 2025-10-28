537f782572f53d937a34eb42ffe503e95aaa95e7
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-10-28 19:43:12 +0100

    cleanup

--- hosts/nixos-server/permissions.nix
+++ hosts/nixos-server/permissions.nix
@@ -14,38 +14,32 @@
       uid = 1001;
       group = "media";
     };
-  users.users.sonarr = {
-    isNormalUser = true;
-    uid = 1002;
-    group = "media";
-#    home = "/var/lib/sonarr";
-#    shell = pkgs.runCommandShell "nologin" {};
+    sonarr = {
+      isNormalUser = true;
+      uid = 1002;
+      group = "media";
+    };
+    lidarr = {
+      isNormalUser = true;
+      uid = 1003;
+      group = "media";
+    };
+    sabnzbd = {
+      isNormalUser = true;
+      uid = 1004;
+      group = "media";
+    };
+    jellyfin = {
+      isNormalUser = true;
+      uid = 1005;
+      group = "media";
+    };
+    paperless = {
+      isNormalUser = true;
+      uid = 1006;
+      group = "paperless";
+    };
   };
-  users.users.lidarr = {
-    isNormalUser = true;
-    uid = 1003;
-    group = "media";
-#    home = "/var/lib/lidarr";
-#    shell = pkgs.runCommandShell "nologin" {};
-  };
-  users.users.sabnzbd = {
-    isNormalUser = true;
-    uid = 1004;
-    group = "media";
-  };
-  users.users.jellyfin = {
-    isNormalUser = true;
-    uid = 1005;
-    group = "media";
-#    home = "/var/lib/jellyfin";
-#    shell = pkgs.runCommandShell "nologin" {};
-  };
-  users.users.paperless = {
-    isNormalUser = true;
-    uid = 1006;
-    group = "paperless";
-  };
-
   # Ensure media directory ownership & permissions
 #  environment.etc."media-permissions.sh".text = ''
 #    chown -R root:media /srv/data1tb/data
