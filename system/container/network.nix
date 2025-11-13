06f04d26d2075eca8e7832371c9f969d42ebe4ac
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-11-13 20:01:31 +0100

    sdf

--- system/container/network.nix
+++ system/container/network.nix
@@ -60,21 +60,6 @@
         "tailscale0"
         "enp1s0"
       ];
-      extraCommands = ''
-      # 1. Enable IP Forwarding (required for NAT to work)
-      echo 1 > /proc/sys/net/ipv4/ip_forward
-      
-      # 2. Accept FORWARD traffic on the bridge (NixOS does some of this declaratively, but this is a good safety net)
-      iptables -A FORWARD -i br-shared -j ACCEPT
-      iptables -A FORWARD -o br-shared -j ACCEPT
-      
-      # 3. Accept traffic related to the bridge (for established connections)
-      iptables -A FORWARD -i br-shared -o enp1s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-      iptables -A FORWARD -i enp1s0 -o br-shared -m state --state RELATED,ESTABLISHED -j ACCEPT
-      
-      # NOTE: The DNAT rules for ports 80, 443, and 53 must be removed/commented out from here.
-      # You should only rely on networking.nat.forwardPorts and the host's nameservers setting.
-    '';
     };
   };
 }
