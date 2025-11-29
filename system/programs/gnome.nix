838874e5b6e8d829b002fb84a92b074c7540c3ea
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2025-11-29 22:17:54 +0100

    gfhg

--- system/programs/gnome.nix
+++ system/programs/gnome.nix
@@ -1,8 +1,12 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
+  ];
 
   environment.gnome.excludePackages = with pkgs; [
     atomix # puzzle game
