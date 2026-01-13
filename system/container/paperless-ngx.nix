5550befc4e5dd3671e59aa840ec9e808fffc90b5
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2026-01-13 11:26:41 +0100

    paperless ip

--- system/container/paperless-ngx.nix
+++ system/container/paperless-ngx.nix
@@ -3,7 +3,7 @@
   containers.paperless = {
     autoStart = true;
     privateNetwork = true;
-     hostAddress = "192.168.100.10";
+    hostAddress = "192.168.100.10";
     localAddress = "192.168.100.11";
     hostAddress6 = "fc00::10";
     localAddress6 = "fc00::11";
