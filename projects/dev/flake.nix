62407001977f52eec81ac3371642fc2ee321178c
Lukas-Angelo Meier <lukasangelo18@gmail.com>
2026-03-13 18:20:16 +0100

    add programming env flake

---
+++ projects/dev/flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Multi-language development environments";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+
+    devShells.${system} = {
+
+      python = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+          python3Packages.pip
+          python3Packages.virtualenv
+          python3Packages.black
+          python3Packages.ipython
+        ];
+      };
+
+      java = pkgs.mkShell {
+        packages = with pkgs; [
+          jdk
+          maven
+          gradle
+        ];
+      };
+
+      cpp = pkgs.mkShell {
+        packages = with pkgs; [
+          gcc
+          clang
+          gdb
+          cmake
+          pkg-config
+        ];
+      };
+
+      rust = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+        ];
+      };
+
+      node = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs
+          nodePackages.npm
+          nodePackages.pnpm
+        ];
+      };
+
+    };
+
+  };
+}
