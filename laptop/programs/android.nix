2e1a56006a47b1c157946ebbd21a64ed9ea4219e
Lukas-Angelo Meier <github.curse332@passmail.com>
2026-04-16 13:23:04 +0200

    fix android studio

--- laptop/programs/android.nix
+++ laptop/programs/android.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
+  nixpkgs.config.android_sdk.accept_license = true;
+
   environment.systemPackages = with pkgs; [
     android-studio
+    android-tools
+    androidenv.androidPkgs.androidsdk
+    jdk17
+    gradle
   ];
-
-  nixpkgs.config = {
-    android_sdk.accept_license = true;
-  };
+  users.users.${username}.extraGroups = [ "kvm" ];
 
 }
